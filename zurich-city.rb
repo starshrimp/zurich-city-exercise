a20b54671eb82d421b2c91cd7e3bd4cb6bed50cc
Sarah Meyer <sarah@mbp-von-sarah.edu.ds.fhnw.ch>
2023-09-27 14:06:54 +0200

    added csv export functionality

--- zurich-city.rb
+++ zurich-city.rb
@@ -1,17 +1,19 @@
 require 'csv'
 
+@table = CSV.table('bev_wegzug_jahr_quartier_seit_2014.csv')
 
-
-table = CSV.table('bev_wegzug_jahr_quartier_seit_2014.csv')
-
-
-p table.headers
-# p table.class
-
-# selected_columns= table.by_col.values_at(:quarlang, :wegktlang)
-# selected_columns[0..10].each do |row|
-#   p row
-# end
+def filter_for_column(column)
+  groups = @table.group_by do |row|
+    row[column]  # Access the value in the specified column for each row
+  end
+  count_hash = groups.transform_values do |rows_in_group|
+    rows_in_group.count  # Count the number of rows in the group
+  end
+  sorted_count_hash = count_hash.sort_by { |_key, value| -value }
+  sorted_count_hash.first(5).each do |value, count|
+    puts "#{value}: #{count}"
+  end
+end
 
 p "Please enter the desired Continent:"
 desired_wegkontinentlang = gets.chomp.to_s
@@ -20,10 +22,8 @@ desired_anzwezuwir = gets.chomp.to_s
 p "Please enter the desired Quartier:"
 desired_quarlang = gets.chomp.to_s
 
-filtered = {}
 
-
-filtered_table = table.select do |row|
+filtered_table = @table.select do |row|
   wegkontinentlang_match = desired_wegkontinentlang.empty? || row[:wegkontinentlang] == desired_wegkontinentlang
   anzwezuwir_match = desired_anzwezuwir.empty? || row[:anzwezuwir].to_s == desired_anzwezuwir
   quarlang_match = desired_quarlang.empty? || row[:quarlang] == desired_quarlang
@@ -31,46 +31,25 @@ filtered_table = table.select do |row|
   wegkontinentlang_match && anzwezuwir_match && quarlang_match
 end
 
-
+puts "You entered: " + desired_wegkontinentlang + " " + desired_anzwezuwir + " " + desired_quarlang
+puts "The following results were found:"
 filtered_table.each_with_index do |row, index|
-  p index.to_s + " " + row[:quarlang] + " " + row[:weglandlang]
+
+  p index.to_s + " " + row[:quarlang].to_s + " " + row[:weglandlang].to_s + " " + row[:anzwezuwir].to_s
 end
 
-
-
-p filtered
-p "You entered: " + desired_wegkontinentlang + " " + desired_anzwezuwir + " " + desired_quarlang
-filter_keys = []
-filtered.each do |key, value|
-  if value == true
-    filter_keys << key
-    p value
-    p "filter key added"
+CSV.open('filtered_rows.csv', 'w') do |csv|
+  filtered_table.each do |row|
+    csv << row
   end
 end
 
-# p filtered 
-# filtered_wegkontinentlang = []
-# table.select do |row| 
-#   filter_keys.each do |key|
-#     if row[key] == desired_wegkontinentlang  row[key] == desired_anzwezuwir || row[key] == desired_quarlang
-#       filtered_wegkontinentlang << row
-#     end
-#   end
-
-#   # if desired_wegkontinentlang == row[:wegkontinentlang] && filtered[:desired_wegkontinentlang] == true
-#   #   filtered_wegkontinentlang << row
-#   # end
-#   # if desired_anzwezuwir =! row[:anzwezuwir]
-#   #   p "row deleted anzwezuwir"
-#   #   filtered_wegkontinentlang.delete(row)
-#   # end
-#   # if desired_quarlang =! row[:quarlang]
-#   #   p "row deleted quarlang"
-#   #   filtered_wegkontinentlang.delete(row)
-#   # end
-# end
-
-filtered_wegkontinentlang.each_with_index do |row, index|
-  p index.to_s + " " + row[:quarlang] + " " + row[:weglandlang]
+print "This are the headers of the table: \n"
+@table.headers.each do |header|
+  print header.to_s + " "
 end
+print "\nEnter the column you want to filter by: "
+column_to_filter = gets.chomp.to_sym
+print "Filtered by #{column_to_filter}: \n"
+filter_for_column(column_to_filter)
+
